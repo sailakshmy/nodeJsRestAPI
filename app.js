bea02854efb7e1ea9ed1ad428b2773c15bd0d1ef
Sai <sailakshmy94@gmail.com>
2025-07-04 14:51:54 +0400

    Created the model for Posts using mongoose and connected to the DB

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const feedRoutes = require("./routes/feed");
 
@@ -16,4 +17,12 @@ app.use((req, res, next) => {
 
 app.use("/feed", feedRoutes);
 
-app.listen(8080);
+const uri =
+  "mongodb+srv://Groot:IAmGroot@cluster0.2ehxgue.mongodb.net/messages?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose
+  .connect(uri)
+  .then(() => {
+    app.listen(8080);
+  })
+  .catch((e) => console.log("error while connecting to mongoose", e));
