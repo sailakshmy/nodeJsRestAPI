35c95e0405473b0532c09fad8feecee89fd16531
Sai <sailakshmy94@gmail.com>
2025-07-04 11:31:25 +0400

    Added a method to handle post creation along with the associated routes

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,1 +1,23 @@
-exports.getPosts = (req, res, next);
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [
+      {
+        title: "first post",
+        content: "This is my first post",
+      },
+    ],
+  });
+};
+
+exports.createPost = (req, res, next) => {
+  const { title, content } = req.body;
+  // Create a post in DB
+  res.status(201).json({
+    message: "Post created successfully!",
+    post: {
+      id: new Date().toISOString(),
+      title,
+      content,
+    },
+  });
+};
--- routes/feed.js
+++ routes/feed.js
@@ -4,5 +4,6 @@ const feedController = require("../controllers/feed");
 const router = express.Router();
 
 router.get("/posts", feedController.getPosts);
+router.post("/post", feedController.createPost);
 
 module.exports = router;
