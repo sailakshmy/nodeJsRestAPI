e7ca22a9e93912288af8bb3a3ddc6134132e07e2
Sai <sailakshmy94@gmail.com>
2025-07-07 09:28:51 +0400

    Updated the posts

--- Frontend/src/pages/Feed/Feed.js
+++ Frontend/src/pages/Feed/Feed.js
@@ -60,7 +60,12 @@ class Feed extends Component {
       })
       .then((resData) => {
         this.setState({
-          posts: resData.posts,
+          posts: resData.posts.map((post) => {
+            return {
+              ...post,
+              imagePath: post.imageUrl,
+            };
+          }),
           totalPosts: resData.totalItems,
           postsLoading: false,
         });
@@ -114,7 +119,8 @@ class Feed extends Component {
     let url = `${BACKEND_URL}/feed/post`;
     let method = "POST";
     if (this.state.editPost) {
-      url = "URL";
+      url = `${url}/${this.state.editPost._id}`;
+      method = "PUT";
     }
 
     fetch(url, {
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const path = require("path");
+
 const { validationResult } = require("express-validator");
 
 const Post = require("../models/post");
@@ -76,5 +79,60 @@ exports.getPost = (req, res, next) => {
       }
 
       next(err);
+    });
+};
+
+const clearImage = (filePath) => {
+  filePath = path.join(__dirname, "..", filePath);
+  fs.unlink(filePath, (err) =>
+    console.log("Error while clearing the image", err)
+  );
+};
+
+exports.updatePost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation failed!");
+    error.statusCode = 422;
+    throw error;
+  }
+  const { postId } = req.params;
+  const { title, content } = req.body;
+  let imageUrl = req.body.image;
+  if (req.file) {
+    imageUrl = req.file.path;
+  }
+  if (!imageUrl) {
+    const error = new Error("No file picked!");
+    error.statusCode = 422;
+    throw error;
+  }
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Could not find the requested post!");
+        error.statusCode = 404;
+        throw error;
+      }
+      if (imageUrl !== post.imageUrl) {
+        clearImage(post.imageUrl);
+      }
+      post.title = title;
+      post.content = content;
+      post.imageUrl = imageUrl;
+      return post.save();
+    })
+    .then((result) => {
+      res.status(200).json({
+        message: "Your post has been updated successfully!",
+        post: result,
+      });
+    })
+    .catch((err) => {
+      if (!e.statusCode) {
+        e.statusCode = 500;
+      }
+
+      next(e);
     });
 };
