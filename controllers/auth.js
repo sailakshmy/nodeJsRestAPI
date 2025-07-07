0c3ad853267783251e31bc27cd6b1f198bfe192d
Sai <sailakshmy94@gmail.com>
2025-07-07 17:19:08 +0400

    Signed up users

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 
 const User = require("../models/user");
 
@@ -11,4 +12,22 @@ exports.signup = (req, res, next) => {
     throw error;
   }
   const { email, name, password } = req.body;
+  bcrypt
+    .hash(password, 12)
+    .then((hashedPassword) => {
+      const user = new User({ email, password: hashedPassword, name });
+      return user.save();
+    })
+    .then((result) => {
+      res.status(201).json({
+        message: "User has been registered successfully!",
+        userId: result._id,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
