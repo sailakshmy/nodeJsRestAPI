7a3b023da3cb9635d1b7fcda10c5361284d78657
Sai <sailakshmy94@gmail.com>
2025-07-08 10:00:37 +0400

    Added the token header to the requests from the client side

--- Frontend/src/pages/Feed/Feed.js
+++ Frontend/src/pages/Feed/Feed.js
@@ -130,6 +130,9 @@ class Feed extends Component {
     fetch(url, {
       method,
       body: formData,
+      headers: {
+        Authorization: `Bearer ${this.props.token}`,
+      },
     })
       .then((res) => {
         if (res.status !== 200 && res.status !== 201) {
@@ -183,6 +186,9 @@ class Feed extends Component {
     this.setState({ postsLoading: true });
     fetch(`${BACKEND_URL}/feed/post/${postId}`, {
       method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${this.props.token}`,
+      },
     })
       .then((res) => {
         if (res.status !== 200 && res.status !== 201) {
--- Frontend/src/pages/Feed/SinglePost/SinglePost.js
+++ Frontend/src/pages/Feed/SinglePost/SinglePost.js
@@ -15,7 +15,11 @@ class SinglePost extends Component {
 
   componentDidMount() {
     const postId = this.props.match.params.postId;
-    fetch(`${BACKEND_URL}/feed/post/${postId}`)
+    fetch(`${BACKEND_URL}/feed/post/${postId}`, {
+      headers: {
+        Authorization: `Bearer ${this.props.token}`,
+      },
+    })
       .then((res) => {
         if (res.status !== 200) {
           throw new Error("Failed to fetch status");
