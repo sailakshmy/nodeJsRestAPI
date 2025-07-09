e49191ebe6c369674c121e497dd3fe0a488cf00e
Sai <sailakshmy94@gmail.com>
2025-07-09 11:07:28 +0400

    Handled errors

--- app.js
+++ app.js
@@ -61,6 +61,19 @@ app.use(
     schema: graphqlSchema,
     rootValue: graphqlResolver,
     graphiql: true,
+    formatError(err) {
+      if (!err.originalError) {
+        return err;
+      }
+      const data = err.originalError.data;
+      const message = err.message ?? "An error occurred";
+      const code = err.originalError.code ?? 500;
+      return {
+        message,
+        code,
+        data,
+      };
+    },
   })
 );
 
