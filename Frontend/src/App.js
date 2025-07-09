47e053e7b914323bdfb32280ea48fc6cee0f7649
Sai <sailakshmy94@gmail.com>
2025-07-09 11:24:53 +0400

    Connected the Frontend to the graphql API

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -111,31 +111,57 @@ class App extends Component {
     console.log("authData", authData);
     event.preventDefault();
     this.setState({ authLoading: true });
-    fetch(`${BACKEND_URL}/auth/signup`, {
-      method: "PUT",
+    // fetch(`${BACKEND_URL}/auth/signup`, {
+    //   method: "PUT",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({
+    //     email: authData.signupForm.email.value,
+    //     password: authData.signupForm.password.value,
+    //     name: authData.signupForm.name.value,
+    //   }),
+    // })
+    //     .then((res) => {
+    //   if (res.status === 422) {
+    //     throw new Error(
+    //       "Validation failed. Make sure the email address isn't used yet!"
+    //     );
+    //   }
+    //   if (res.status !== 200 && res.status !== 201) {
+    //     console.log("Error!");
+    //     throw new Error("Creating a user failed!");
+    //   }
+    //   return res.json();
+    // })
+    const graohqlQuery = {
+      query: `
+        mutation {
+          createUser(userInput:{email:"${authData.signupForm.email.value}", name: "${authData.signupForm.name.value}", password:"${authData.signupForm.password.value}"}){
+            _id
+            email
+          }
+        }
+      `,
+    };
+    fetch(`${BACKEND_URL}/graphql`, {
+      method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-        email: authData.signupForm.email.value,
-        password: authData.signupForm.password.value,
-        name: authData.signupForm.name.value,
-      }),
+      body: JSON.stringify(graohqlQuery),
     })
-      .then((res) => {
-        if (res.status === 422) {
+      .then((res) => res.json())
+      .then((resData) => {
+        console.log(resData);
+        if (resData.errors && resData.errors[0].status === 422) {
           throw new Error(
             "Validation failed. Make sure the email address isn't used yet!"
           );
         }
-        if (res.status !== 200 && res.status !== 201) {
-          console.log("Error!");
+        if (resData.errors) {
           throw new Error("Creating a user failed!");
         }
-        return res.json();
-      })
-      .then((resData) => {
-        console.log(resData);
         this.setState({ isAuth: false, authLoading: false });
         this.props.history.replace("/");
       })
--- app.js
+++ app.js
@@ -47,8 +47,14 @@ app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,PATCH,DELETE,OPTIONS"
+  );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
   next();
 });
 
